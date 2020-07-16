41f9bbf494c2233b43b978873beafda09adfcb53
Joel Berman <joel.berman@gmail.com>
2020-07-15 20:00:14 -0700

    update

--- daily.sh
+++ daily.sh
@@ -4,6 +4,6 @@ Rscript daily.R
 Rscript -e "require ('rmarkdown'); render('daily.Rmd')"
 mv daily.html index.html
 git add index.html
-git commit -m $NOW
+git commit -a -m $NOW
 git push -u origin master
 
