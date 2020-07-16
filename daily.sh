cc95cbb28ac1fe52f5399e7f0630cf461b3a9478
Joel Berman <joel.berman@gmail.com>
2020-07-15 19:30:20 -0700

    test

--- daily.sh
+++ daily.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+NOW=$( date '+%F_%H:%M:%S' )
 Rscript daily.R
 Rscript -e "require ('rmarkdown'); render('daily.Rmd')"
+mv daily.html index.html
+git  add index.html
+git commit -m $NOW
+git push origin master
 
