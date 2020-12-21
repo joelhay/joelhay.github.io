ed97c01b880813c8239360618071adee1fa5e56c
Ubuntu <ubuntu@ip-172-31-1-74.us-west-1.compute.internal>
2020-12-21 06:53:07 +0000

    Added scipt for ubuntu

---
+++ ubuntudaily.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd
+cd joelhay.github.io
+NOW=$( date '+%F_%H:%M:%S' )
+Rscript daily.R
+Rscript -e "require ('rmarkdown'); render('daily.Rmd')"
+mv daily.html index.html
+git add index.html
+git commit -a -m $NOW
+git push -u origin master
+
